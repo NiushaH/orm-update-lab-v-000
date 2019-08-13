4f602fadf713e06b5501bcc96fe5674f2b345fe6
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-13 05:16:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,7 +74,7 @@ class Student
   end
   
   def self.update
-    sql = "UPDATE songs SET name = ?, grade = ? WHERE id = ?"
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end 
 
